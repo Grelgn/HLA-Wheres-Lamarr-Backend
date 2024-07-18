1adad40b7cc3d549e20f8dad864838f563a4bfed
Grelgn <grelgn@hotmail.com>
2024-07-18 19:00:56 +0300

    Remove levels from DB as they weren't needed

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const logger = require("morgan");
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -36,7 +35,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 //  Require controller modules
-const levelController = require("../controllers/levelController");
 const recordController = require("../controllers/recordController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
 	res.render("index", { title: "Express" });
 });
-
-// Level
-router.get("/level/:name", levelController.getLevel);
-router.get("/levels", levelController.getAllLevels);
 
 // Record
 router.get("/records", recordController.getAllRecords);
